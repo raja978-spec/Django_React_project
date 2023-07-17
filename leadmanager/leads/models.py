20c7a8ec5d1b81d15d29784a09dea1de03754f48
raja-spec <rraja48569@gmail.com>
2023-07-17 21:22:58 +0530

    Adding serializer

--- leadmanager/leads/models.py
+++ leadmanager/leads/models.py
@@ -5,4 +5,4 @@ class Lead(models.Model):
     name=models.CharField(max_length=100)
     email=models.EmailField(max_length=100,unique=True)
     message=models.CharField(max_length=500,blank=True)
-    created_at=models.DateTimeField(auto_now_add=True)
+    created_at=models.DateTimeFieldf(auto_now_add=True)
