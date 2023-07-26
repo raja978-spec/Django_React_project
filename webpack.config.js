04dcf584ac6626901f541bed094d2995828aa184
raja-spec <rraja48569@gmail.com>
2023-07-26 22:07:12 +0530

    Config package file

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,24 @@
-module.exports={
-    module:{
-        rules:[
-            {
-                test:/\.js$/,
-                exclude:/node_modules/,
-                use:{
-                    loader:"babel-loder"
-                }
-            }
-        ]
-    }
-}
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'leadmanager', 'frontend', 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'leadmanager', 'frontend', 'static', 'frontend'),
+    filename: 'main.js',
+  },
+  resolve: {
+    modules: [path.resolve(__dirname, 'leadmanager', 'frontend', 'src'), 'node_modules'],
+    extensions: ['.js', '.json'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+};
